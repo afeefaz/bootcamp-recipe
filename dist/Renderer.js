223364f468d0e127f5e3f9e74a16745ba8a7221e
Afeef <afeef.as11@gmail.com>
2021-05-24 12:45:01 +0300

    Done

---
+++ dist/Renderer.js
@@ -0,0 +1,14 @@
+class Renderer {
+    constructor() {}
+
+    render(data) {
+        $('.container').empty();
+
+        const source = $("#recipes-template").html()
+        const template = Handlebars.compile(source)
+        
+        const newHTML = template({results: data})
+        
+        $('.container').append(newHTML)
+    }
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const request = require('request')
+
+const app = express()
+const port = 8080
+let recipes = []
+
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+
+app.get('/recipes/:ingredient', function (req, res) {
+    let ingredient = req.params.ingredient
+    
+    request(`https://recipes-goodness.herokuapp.com/recipes/${ingredient}`, function(err, response, data){
+        recipes = JSON.parse(data).results.map(recipe => {return {title: recipe.title,thumbnail: recipe.thumbnail,href:recipe.href,ingredients:recipe.ingredients}})
+
+        res.send(recipes)
+    })
+});
+
+app.listen(port, function(){
+    console.log(`Running server on port ${port}`)
+})
